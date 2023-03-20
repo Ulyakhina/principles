54519b7ed7ee06211cbdb3c60d991f078b4d1832
Anastasiya Ulyakhina <ulyakhina.a@gmail.com>
2023-03-20 17:48:59 +0300

    first commit

---
+++ src/main/java/ru/netology/post/FormDate.java
@@ -0,0 +1,11 @@
+package ru.netology.post;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+    //"birthday": {
+    //    "day": 13,
+    //    "month": 6,
+    //    "year": 1999
+}
